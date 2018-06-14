c346a40f22fa61c3bf07b846265cb111796b5829
Benjamin <ben.smith.gordon@gmail.com>
2018-06-14 12:14:48 +0100

    Current stage

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,17 +2,15 @@ require_relative "bike"
 
 class DockingStation
 
-  def bike
+attr_reader :bike
 
+  def bike
+    @bike
   end
 
 
   def release_bike
-    Bike.new
-  end
-
-  def dock(bike)
-  bike
+    bike = Bike.new
   end
 end
 
